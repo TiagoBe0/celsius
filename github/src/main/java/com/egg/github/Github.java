b00a2d22ea6c2919cc765e43e470a88ada327d57
TiagoBe0 <santiagobergamin@gmail.com>
2024-08-23 21:29:54 -0300

    primer push

---
+++ github/src/main/java/com/egg/github/Github.java
@@ -0,0 +1,26 @@
+
+package com.egg.github;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Github {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        ArrayList<String> nombres = new ArrayList();
+        
+        for (String nombre : nombres) {
+            
+            //nombre = "julian";
+            nombre = leer.next();
+            nombres.add(leer.next());
+            
+        }
+        for (String nombre : nombres) {
+            
+            System.out.println("nombre: "+nombre);
+            
+        }
+    }
+}
